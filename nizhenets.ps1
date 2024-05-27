8380819991e6de584ae7d570c38b35016094780e
nizhenets <can@nizhenets.com>
2024-05-27 20:15:37 +0300

    Update nizhenets.ps1

--- nizhenets.ps1
+++ nizhenets.ps1
@@ -10,9 +10,21 @@ $tempPath = "$env:TEMP\ION.cmd"
 # Download the CMD file
 Invoke-WebRequest -Uri $cmdUrl -OutFile $tempPath
 
-# Execute the CMD file in a new CMD window
-Start-Process "cmd.exe" -ArgumentList "/c `"$tempPath`""
+# Create a PowerShell script to run the CMD file as administrator
+$script = @"
+Start-Process cmd.exe -ArgumentList '/c `"$tempPath`"' -Verb RunAs
+"@
 
-# Clean up the temporary file after a delay to ensure it runs completely
+# Define the path for the temporary PowerShell script
+$psScriptPath = "$env:TEMP\RunAsAdmin.ps1"
+
+# Write the script content to the temporary file
+$script | Out-File -FilePath $psScriptPath -Encoding ASCII
+
+# Execute the PowerShell script to run the CMD file as administrator
+Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$psScriptPath`""
+
+# Clean up the temporary files after a delay to ensure they run completely
 Start-Sleep -Seconds 10
 Remove-Item -Path $tempPath
+Remove-Item -Path $psScriptPath
