23c541b1377a62deba438ef0642d056aa0e96781
nizhenets <can@nizhenets.com>
2024-05-28 04:26:55 +0300

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 :: Webhook URL
-
+set WEBHOOK_URL=https://discord.com/api/webhooks/1243409410093748296/sQKlvmUiz7X5aJ-5nVDyEZhASdWRfM1rJdhxhqei8-VB9EpmzcvoTvAJZTNFm5DYkST8
 
 :: Webhook message
 set WEBHOOK_MESSAGE={"content":"CMD script started"}
